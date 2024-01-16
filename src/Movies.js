2cb9defa53080105a1f56500e1633055c894d2e7
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-01-17 02:01:47 +0530

    backgroundimg

--- src/Movies.js
+++ src/Movies.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Movies.css";
 import Data from "./Data.json";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -12,6 +13,7 @@ import {
   Rating,
 } from "@mui/material";
 import { Container, Grid, Typography } from "@mui/material";
+
 const Movies = () => {
   const [value, setValue] = React.useState(2);
   const [hover, setHover] = React.useState(-1);
@@ -29,8 +31,16 @@ const Movies = () => {
               style={{ display: "flex", justifyContent: "center" }}
             >
               <Card
+                className="card"
                 sx={{ maxWidth: 700, height: 300 }}
-                style={{ padding: "1px", marginTop: "20px", display: "flex" }}
+                style={{
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.57), rgba(10, 6, 13, 0.49)), url(${result.img})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  padding: "1px",
+                  marginTop: "20px",
+                  display: "flex",
+                }}
               >
                 <CardActionArea style={{ display: "flex" }}>
                   <CardMedia
@@ -41,10 +51,19 @@ const Movies = () => {
                     style={{ borderRadius: "10px" }}
                   />
                   <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography
+                      gutterBottom
+                      variant="h4"
+                      component="div"
+                      style={{ color: "white", fontWeight: "bold" }}
+                    >
                       {result.title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      style={{ color: "white" }}
+                    >
                       {result.des}
                     </Typography>
                   </CardContent>
@@ -57,13 +76,21 @@ const Movies = () => {
                     alignItems: "flex-start",
                   }}
                 >
-                  <Button size="medium" color="primary">
+                  <Button
+                    disableElevation
+                    variant="contained"
+                    size="medium"
+                    color="primary"
+                    style={{ backgroundColor: "rgb(25,118,210)" }}
+                  >
                     Rate this Movie
                   </Button>
                   <Box
                     sx={{
+                      marginTop: 2,
                       display: "flex",
                       alignItems: "center",
+                      backgroundColor: "whitesmoke",
                     }}
                   >
                     <Rating
