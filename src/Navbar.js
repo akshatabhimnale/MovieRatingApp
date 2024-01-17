b2608537056673565b81d3202319cd5fbc8a7d62
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-01-17 21:33:06 +0530

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,62 @@
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
 //import MovieIcon from "@mui/icons-material/Movie";
-import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  InputBase,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  alpha,
+  styled,
+} from "@mui/material";
 
 import { MenuBook, MenuOpen } from "@mui/icons-material";
 import { GridMenuIcon } from "@mui/x-data-grid";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 const Navbar = () => {
   return (
     <React.Fragment>
@@ -11,6 +64,7 @@ const Navbar = () => {
         <Toolbar>
           <GridMenuIcon />
           <Typography> Movie Rating</Typography>
+
           <Tabs sx={{ marginLeft: "auto" }} textColor="inherit">
             <Tab label="Search" />
             <Tab label="Home" />
