d4b11e34b92c3b1ea570f814d97a9b2491140012
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-01-17 20:21:19 +0530

    menu icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,11 +3,13 @@ import React from "react";
 import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 
 import { MenuBook, MenuOpen } from "@mui/icons-material";
+import { GridMenuIcon } from "@mui/x-data-grid";
 const Navbar = () => {
   return (
     <React.Fragment>
       <AppBar sx={{ background: "#063970" }}>
         <Toolbar>
+          <GridMenuIcon />
           <Typography> Movie Rating</Typography>
           <Tabs sx={{ marginLeft: "auto" }} textColor="inherit">
             <Tab label="Search" />
